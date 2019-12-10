1b4d076d11ec08c272ae950b0a84c3bbfda0743a
s-301-06 <s-301-06@gumiforest.uek.krakow.pl>
2019-12-10 11:56:16 +0100

    commit

--- TPI_25.11.19/quadratic_equation.py
+++ TPI_25.11.19/quadratic_equation.py
@@ -16,8 +16,8 @@ else:
         x = -b / (2 * a)
         print(f"x0 = {x}")
     elif delta > 0:
-        x1 = (-b - delta ** 1/2) / (2 * a)
-        x2 = (-b + delta ** 1/2) / (2 * a)
+        x1 = (-b - delta ** (1/2)) / (2 * a)
+        x2 = (-b + delta ** (1/2)) / (2 * a)
         print(f"x1 = {x1}, x2 = {x2}")
     else:
         print("Nie ma miejsc zerowych.")
